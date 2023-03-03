3824e313857c0e25493d5d7fe3f63d9b0f0b64ab
emmanuelkiplangatbett <emmanuel.bett@student.moringa>
2023-03-03 13:42:02 +0300

    added changes on my aplication_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,119 +6,90 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
-  # post "/projects/create" do
-  #   data = request.body.read
-  #   data.to_s
-  # end
+  # Fetch all users from the database
   get '/users' do
     user = User.all
     user.to_json
   end
 
-  # post '/users' do
-  #   user = User.create(params)
-  # end
-  post 'users/create' do
-    data = request.body.read
-    data.to_s
+  # Add a new user to the database
+  post '/users' do
+    user = User.create(params)
   end
 
+
+
+# Fetch all projects from the database
+  get '/projects' do
+    project = Project.all
+    project.to_json
+  end
+
+  # Add a new project to the database
+  post '/projects' do
+    project = Project.create(params)
+    project.to_json
+  end
+
+ # Fetch project by :id from the database
+  get '/projects/:id' do
+    project = Project.find(params[:id])
+    project.to_json
+  end
+
+   # Update project data from the database
+  patch '/projects/:id' do
+    project = Project.find(params[:id])
+    project.update(
+      title: params[:title],
+      project_description: params[:project_description],
+      project_language: params[:project_language]
+    )
+    project.to_json
+  end
+
+  # Delete a project by :id from the database
+  delete '/projects/:id' do
+    project = Project.find(params[:id])
+    project.destroy
+    project.to_json
+  end
+  
+ # Fetch all skills from the database
   get '/skills' do
     skill = Skill.all
     skill.to_json
   end
 
+  # Add a new skill to the database
   post '/skills' do
-    skill = Skill.create_skill(params)
+    skill = Skill.create(params)
     skill.to_json
   end
 
-  
-
-  get '/projects' do
-    project = Project.all
-    project.to_json
+   # Update a skill by :id from the database
+  patch '/skills/:id' do
+    skill = Skill.find(params[:id])
+    skill.update(
+      skill_name: params[:skill_name]
+    )
+    skill.to_json
   end
+
+# Delete a skill by :id from the database
+  delete '/skills/:id' do
+    skill = Skill.find(params[:id])
+    skill.destroy
+    skill.to_json
+  end
+
 end
 
 
 
-#   # define routes
-# get '/' do
-#   erb :index
-# end
 
-# get '/register' do
-#   erb :register
-# end
 
-# post '/register' do
-#   # handle user registration
-# end
 
-# get '/login' do
-#   erb :login
-# end
-
-# post '/login' do
-#   # handle user login
-# end
-
-# get '/logout' do
-#   # handle user logout
-# end
-
-# get '/dashboard' do
-#   # handle user dashboard
-# end
-
-# get '/projects' do
-#   # handle project listing
-# end
-
-# get '/projects/new' do
-#   # handle new project form
-# end
-
-# post '/projects/new' do
-#   # handle new project submission
-# end
-
-# get '/projects/:id/edit' do
-#   # handle project edit form
-# end
-
-# post '/projects/:id/edit' do
-#   # handle project update submission
-# end
-
-# post '/projects/:id/delete' do
-#   # handle project deletion
-# end
-
-# get '/skills' do
-#   # handle skill listing
-# end
-
-# get '/skills/new' do
-#   # handle new skill form
-# end
-
-# post '/skills/new' do
-#   # handle new skill submission
-# end
-
-# get '/skills/:id/edit' do
-#   # handle skill edit form
-# end
-
-# post '/skills/:id/edit' do
-#   # handle skill update submission
-# end
-
-# post '/skills/:id/delete' do
-#   # handle skill deletion
-# end
 # # Home page
 # require 'sinatra'
 # require 'sinatra/activerecord'
